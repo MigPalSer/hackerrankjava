c8c5a4b4efa7a9cafcb27523b294f2682091d6d4
mps <migpalser@gmail.com>
2021-01-29 08:33:38 +0100

    reto arrayList

--- src/retos/java1/arraylist/Solution.java
+++ src/retos/java1/arraylist/Solution.java
@@ -1,7 +1,7 @@
 package retos.java1.arraylist;
+
 import java.io.*;
 import java.util.*;
-
 
 /*https://www.hackerrank.com/challenges/java-arraylist/problem
 *INPUT: The first line has an integer n. In each of the next n lines there will 
@@ -14,35 +14,35 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-       Scanner mainScanner=new Scanner(System.in);
-       int linesNumber=mainScanner.nextInt();
-       ArrayList<ArrayList<Integer>> listOfLines=new ArrayList<ArrayList<Integer>>(linesNumber);
-       for (int i = 0; i < linesNumber; i++) {
-		int lineAmountIntegers=mainScanner.nextInt();
-		ArrayList<Integer> lineArray=new ArrayList<Integer>(lineAmountIntegers);
-		for (int j = 0; j < lineAmountIntegers; j++) {
-			lineArray.add(mainScanner.nextInt());
+	public static void main(String[] args) {
+		Scanner mainScanner = new Scanner(System.in);
+		int linesNumber = mainScanner.nextInt();
+		ArrayList<ArrayList<Integer>> listOfLines = new ArrayList<ArrayList<Integer>>(linesNumber);
+		for (int i = 0; i < linesNumber; i++) {
+			int lineAmountIntegers = mainScanner.nextInt();
+			ArrayList<Integer> lineArray = new ArrayList<Integer>(lineAmountIntegers);
+			for (int j = 0; j < lineAmountIntegers; j++) {
+				lineArray.add(mainScanner.nextInt());
+			}
+			listOfLines.add(lineArray);
 		}
-		listOfLines.add(lineArray);
-	}
-       int querys=mainScanner.nextInt();
-       for (int i = 0; i < querys; i++) {
-           int x=mainScanner.nextInt();
-           int y=mainScanner.nextInt();
-           
-          if (x<=listOfLines.size()) {
-			if (y<=listOfLines.get(x-1).size()) {
-				System.out.println(listOfLines.get(x-1).get(y-1));
-			}else {
+		int querys = mainScanner.nextInt();
+		for (int i = 0; i < querys; i++) {
+			int x = mainScanner.nextInt();
+			int y = mainScanner.nextInt();
+
+			if (x <= listOfLines.size()) {
+				if (y <= listOfLines.get(x - 1).size()) {
+					System.out.println(listOfLines.get(x - 1).get(y - 1));
+				} else {
+					System.out.println("ERROR!");
+				}
+			} else {
 				System.out.println("ERROR!");
 			}
-		}else {
-			System.out.println("ERROR!");
+
 		}
 
+		mainScanner.close();
 	}
-       
-	   mainScanner.close();
-    }
 }
