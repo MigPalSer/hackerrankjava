2588fd0d245948d6b488c9ce130fd719444919f0
mps <migpalser@gmail.com>
2021-02-15 18:24:30 +0100

    minimum height triangle challenge

--- src/retos/mathematics/smalltriangle/Solution.java
+++ src/retos/mathematics/smalltriangle/Solution.java
@@ -12,18 +12,20 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static int lowestTriangle(int base, int area){
-    	//FAILING METHOD - TO CORRECT
-        Double height= (((double)area*2)/(double)base);
-        height=Math.rint(height);
-        return height.intValue();
-    }
+	static int lowestTriangle(int base, int area) {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int base = in.nextInt();
-        int area = in.nextInt();
-        int height = lowestTriangle(base, area);
-        System.out.println(height);
-    }
+		int height = (area * 2) / base;
+		if ((area * 2) % base != 0) {
+			height++;
+		}
+		return height;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int base = in.nextInt();
+		int area = in.nextInt();
+		int height = lowestTriangle(base, area);
+		System.out.println(height);
+	}
 }
