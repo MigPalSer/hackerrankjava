53bf49810ad3cc1f23d5aae811fdae8b6c1a6e5c
mps <migpalser@gmail.com>
2021-04-24 08:56:15 +0200

    socket merchant interview challenge

---
+++ src/retos/interview/sockmerchant/Solution.java
@@ -0,0 +1,58 @@
+package retos.interview.sockmerchant;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/**/
+
+public class Solution {
+
+    static int sockMerchant(int n, int[] ar) {
+       HashSet<Integer> oddSocks=new HashSet<Integer>();
+       int pairs=0;
+       
+       for (Integer integer : ar) {
+		if(oddSocks.contains(integer)) {
+			pairs++;
+			oddSocks.remove(integer);
+		}else {
+			oddSocks.add(integer);
+		}
+	}
+       return pairs;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
