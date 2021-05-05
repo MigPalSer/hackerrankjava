8782c468d1b2898137d4fd9d6d9f6d94f2d57bc2
mps <migpalser@gmail.com>
2021-05-05 06:15:39 +0200

    new year chaos interview challenge

--- src/retos/interview/newyearchaos/Solution.java
+++ src/retos/interview/newyearchaos/Solution.java
@@ -15,7 +15,7 @@ import static java.util.stream.Collectors.toList;
 /*https://www.hackerrank.com/challenges/new-year-chaos/problem
  *TASK: with the rules of the challenge, print the number of bribes to obtain a
  *certain queue. 
- *TODO: Solve the time limit problem
+ *The code is a bit complex because I needed to solve the time limit problem
  * */
 
 class Result {
@@ -23,14 +23,25 @@ class Result {
     public static void minimumBribes(List<Integer> q) {
     int bribes=0;
     boolean toochaotic=false;
-	HashSet<Integer> ticketspresentes=new HashSet<Integer>();
+	//We catch every number that appear at the queue
+    HashSet<Integer> ticketspresentes=new HashSet<Integer>();
+	//Lowest index for the internal loop, we will up it for shorting that loop
+    int index=0;
 
     for (int i = 0; i < q.size(); i++) {
 		int numero=q.get(i);
+		//Test the chaotic condition and, if it's true, break the loop
+		if(numero-i>3)toochaotic=true;
+		if(toochaotic)break;
 		ticketspresentes.add(numero);
-		if(q.get(i)-i>3)toochaotic=true;
-		for (int j = numero-1; j >0; j--) {
-			if(!ticketspresentes.contains(j))bribes++;
+		//Internal loop, for every lower number that NOT appear in the previous numbers
+		//a bribe (by actual person or not) must be did.
+		for (int j = numero-1; j >index; j--) {
+			if(!ticketspresentes.contains(j)) {bribes++;
+			//If the last number appear, we can up the index for shorting our loop
+			}else if(j==index+1) {
+				index++;
+			}
 		}
 	}
     
